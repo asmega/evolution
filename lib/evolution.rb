e1637728f1728ea6c1efc6d93d147942edd4ebe0
asmega <asmega@ph-lee.com>
2012-07-07 23:49:32 +0100

    added first basic implementation

--- lib/evolution.rb
+++ lib/evolution.rb
@@ -1,5 +1,42 @@
 require "evolution/version"
 
 module Evolution
-  # Your code goes here...
+  class Repo
+    def initialize(options = {})
+      @path = options[:path] || Dir.pwd
+      @results = {}
+      @before = options[:before]
+      @after = options[:after] || (Date.today << 6).strftime('%Y-%m-%d')
+    end
+
+    def run
+      files = []
+
+      Dir.chdir(@path) do
+        files = `git log --after #{@after} --name-only --format='%n'`.split(/\n/).reject{|line| line == ""}
+      end
+
+      results = {}
+
+      files.each do |file|
+        # TODO This is not optimal
+        results[file] = files.count(file)
+      end
+
+      @results = results.sort_by{|r| r[1]}.reverse
+    end
+
+    def report
+      max_file_length = @results.map{|r| r[0].length}.max
+
+      puts "+---------------+#{'-' * max_file_length}---+"
+      puts "| Times Changed | File path #{' ' * (max_file_length - 8) }+"
+      puts "+---------------+#{'-' * max_file_length}---+"
+      @results.each do |result|
+        puts "| #{result[1]}#{" " * (13 - result[1].to_s.length)} | #{result[0]}#{" " * (max_file_length - result[0].to_s.length + 2)}|"
+      end
+      puts "+---------------+#{'-' * max_file_length}---+"
+      nil
+    end
+  end
 end
